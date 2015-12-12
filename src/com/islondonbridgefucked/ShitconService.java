6bafde2f4324c1dc01f3b784531da8e6e5e77895
Filip Wieland <filiplamparski@gmail.com>
2015-12-12 23:35:16 +0000

    Receive train data!

--- src/com/islondonbridgefucked/ShitconService.java
+++ src/com/islondonbridgefucked/ShitconService.java
@@ -1,19 +1,16 @@
 package com.islondonbridgefucked;
 
-import java.util.Collections;
-import java.util.zip.GZIPInputStream;
-
-import javax.jms.BytesMessage;
-import javax.jms.Message;
-
-import org.apache.activemq.util.ByteArrayInputStream;
+import java.util.List;
 
 import com.islondonbridgefucked.configuration.FuckingConfiguration;
 import com.islondonbridgefucked.datafeeds.DarwinDataFeed;
-import com.sun.el.stream.Stream;
+import com.thalesgroup.pushport.Pport;
+import com.thalesgroup.pushport.StationMessage;
+import com.thalesgroup.pushport.TS;
+import com.thalesgroup.pushport.TrainAlert;
+import com.thalesgroup.pushport.Schedule;
 
 import io.dropwizard.Application;
-import io.dropwizard.jersey.gzip.GZipDecoder;
 import io.dropwizard.setup.Bootstrap;
 import io.dropwizard.setup.Environment;
 
@@ -37,19 +34,23 @@ public class ShitconService extends Application<FuckingConfiguration> {
 	@Override
 	public void run(FuckingConfiguration conf, Environment env) throws Exception {
 		this.feed = new DarwinDataFeed("tcp://datafeeds.nationalrail.co.uk:61616", conf.getDarwinConfiguration().getQueue());
-		feed.setListener((Message msg) -> {
-			BytesMessage bm = (BytesMessage) msg;
-			try {
-				byte[] bytes = new byte[(int) bm.getBodyLength()];
-				bm.readBytes(bytes);
-				
-				byte[] decoded = new byte[(int) bm.getBodyLength() * 2];
-				GZIPInputStream gis = new GZIPInputStream(new ByteArrayInputStream(bytes));
-				gis.read(decoded);
-				System.out.println(new String(decoded));
-			} catch (Exception e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
+		feed.setListener((Pport port) -> {
+			System.out.println(port.getTs().toString() + ": " + port.getUR().getUpdateOrigin() + ":");
+			if (port.getUR().getTS().size() > 0) {
+				List<TS> tss = port.getUR().getTS();
+				System.out.println("  TrainStatus x " + tss.size());
+			}
+			if (port.getUR().getOW().size() > 0) {
+				List<StationMessage> sms = port.getUR().getOW();
+				System.out.println("  StationMessage x " + sms.size());
+			}
+			if (port.getUR().getSchedule().size() > 0) {
+				List<Schedule> ss = port.getUR().getSchedule();
+				System.out.println("  Schedule x " + ss.size());
+			}
+			if (port.getUR().getTrainAlert().size() > 0) {
+				List<TrainAlert> ats = port.getUR().getTrainAlert();
+				System.out.println("  TrainAlert x " + ats.size());
 			}
 		});
 		feed.start();
--- src/com/islondonbridgefucked/datafeeds/DarwinDataFeed.java
+++ src/com/islondonbridgefucked/datafeeds/DarwinDataFeed.java
@@ -1,5 +1,14 @@
 package com.islondonbridgefucked.datafeeds;
 
+import java.io.BufferedOutputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.PrintStream;
+import java.util.zip.GZIPInputStream;
+
+import javax.jms.BytesMessage;
 import javax.jms.Connection;
 import javax.jms.JMSException;
 import javax.jms.Message;
@@ -8,23 +17,32 @@ import javax.jms.MessageListener;
 import javax.jms.Queue;
 import javax.jms.Session;
 import javax.jms.Topic;
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBElement;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Unmarshaller;
 
 import org.apache.activemq.ActiveMQConnectionFactory;
 import org.apache.activemq.command.ActiveMQQueue;
 import org.apache.activemq.command.ActiveMQTopic;
+import org.apache.activemq.util.ByteArrayInputStream;
 
-public class DarwinDataFeed {
+import com.thalesgroup.pushport.Pport;
+
+public class DarwinDataFeed implements MessageListener {
 	private final ActiveMQConnectionFactory amqFactory;
 	private final Connection conn;
 	private final MessageConsumer receiver;
 	private DarwinOnMessage listener;
 	
+	// private BufferedOutputStream debugStream;
+	
 	@FunctionalInterface
-	public interface DarwinOnMessage extends MessageListener {
-		public void onMessage(Message msg);
+	public interface DarwinOnMessage {
+		public void onMessage(Pport msg);
 	}
 	
-	public DarwinDataFeed(String brokerUrl, String queue) throws JMSException {
+	public DarwinDataFeed(String brokerUrl, String queue) throws JMSException, FileNotFoundException {
 		// Generic values. The real password is the queue name.
 		amqFactory = new ActiveMQConnectionFactory("d3user", "d3password", brokerUrl);
 		conn = amqFactory.createConnection();
@@ -32,14 +50,44 @@ public class DarwinDataFeed {
 		Session sess = conn.createSession(false, Session.AUTO_ACKNOWLEDGE);
 		Queue topic = new ActiveMQQueue(queue);
 		receiver = sess.createConsumer(topic);
+		receiver.setMessageListener(this);
+		
+		// debugStream = new BufferedOutputStream(new FileOutputStream("/tmp/data-capture"));
 	}
 	
-	public void setListener(DarwinOnMessage l) throws JMSException {
+	public void setListener(DarwinOnMessage l) {
 		listener = l;
-		receiver.setMessageListener(listener);
 	}
 	
 	public void start() throws JMSException {
 		conn.start();
+	}
+	
+	private InputStream decodeMessage(BytesMessage bmsg, int len) throws JMSException, IOException {
+		byte[] bytes = new byte[len];
+		bmsg.readBytes(bytes);
+		
+		GZIPInputStream gis = new GZIPInputStream(new ByteArrayInputStream(bytes));
+		return gis;
+	}
+	
+	private Pport unfuckXML(InputStream is) throws JAXBException {
+		String packageName = Pport.class.getPackage().getName();
+		JAXBContext jcx = JAXBContext.newInstance(packageName);
+		Unmarshaller u = jcx.createUnmarshaller();
+		return (Pport) u.unmarshal(is);
+	}
+
+	@Override
+	public void onMessage(Message msg) {
+		try {
+			BytesMessage bmsg = (BytesMessage) msg;
+			int len = (int) bmsg.getBodyLength();
+			Pport parsed = unfuckXML(decodeMessage(bmsg, len));
+			listener.onMessage(parsed);
+		} catch (JMSException | IOException | JAXBException e) {
+			System.err.println("O SHI-");
+			e.printStackTrace();
+		}
 	}
 }
